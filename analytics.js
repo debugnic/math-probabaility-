ce63c95ff174fb71dfaf7412c95bc453dfcc70ff
debugnic <nicholasws.pc@gmail.com>
2026-05-04 18:53:10 +0700

    delete

--- analytics.js
+++
@@ -1,6 +0,0 @@
-// Vercel Web Analytics
-// Import and inject analytics using ES modules
-import { inject } from 'https://cdn.jsdelivr.net/npm/@vercel/analytics@1/dist/index.mjs';
-
-// Initialize analytics
-inject();
